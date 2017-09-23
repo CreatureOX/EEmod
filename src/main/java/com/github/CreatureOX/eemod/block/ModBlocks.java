c72a4d2193201b723e89f52fd7ddbb988cf327ac
贺一凡 <785862780@qq.com>
2017-09-23 13:34:50 +0800

    Dev for Minecraft
    An attempt to dev mcmod

---
+++ src/main/java/com/github/CreatureOX/eemod/block/ModBlocks.java
@@ -0,0 +1,13 @@
+package com.github.CreatureOX.eemod.block;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+
+public class ModBlocks {
+	
+	public static Block experimentBlock;
+	
+	public static final void init(){
+	}
+
+}
